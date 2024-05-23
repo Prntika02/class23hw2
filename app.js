2c496ca0a53f17b044445d6a5a526817372ed522
Prntika02 <159702688+Prntika02@users.noreply.github.com>
2024-05-23 18:34:47 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+let clr = document.querySelector('.clr');
+let btn = document.querySelector('.btn');
+
+btn.addEventListener('click', ()=>
+{
+    let red = Math.round (Math.random()*255);
+    let green = Math.round (Math.random()*255);
+    let blue = Math.round (Math.random()*255);
+
+    clr.style.background = `rgb(${red},${green},${blue})`
+});
